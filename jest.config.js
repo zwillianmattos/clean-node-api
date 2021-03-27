2e861f651ae78addf5fac72031d3b1b7dca9aef1
willian mattos <willian_matt@hotmail.com>
2021-03-27 18:00:19 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  roots: ['<rootDir>/src/'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'], // Cobertura de teste de arquivos
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
