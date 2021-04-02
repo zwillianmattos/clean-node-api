360661edcf248d5b48cc01b939254657c51ba023
willian mattos <willian_matt@hotmail.com>
2021-04-02 20:38:20 -0300

    refactor: connect to mongodb before integration tests

---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+export default {
+    MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+    PORT: process.env.PORT || 5050
+}
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,11 @@
+import { MongoHelper } from "../infra/db/mongodb/helpers/mongo-helper";
 import app from "./config/app";
+import env from "./config/env";
 
-app.listen(5050, () => console.log("Running 5050"))
+
+MongoHelper.connect(env.MONGO_URL)
+    .then( async () => {
+        const app = (await import("./config/app")).default
+
+        app.listen(env.PORT, () => console.log(`Running ${env.PORT}`))
+    }).catch(console.error)
