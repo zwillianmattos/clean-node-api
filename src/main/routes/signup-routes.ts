023c2b90b6184f8b71798566445ef0b37cd5cea5
willian mattos <willian_matt@hotmail.com>
2021-04-02 20:43:59 -0300

    feat: add SignUp Factory

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,9 +1,5 @@
 import { Router } from "express";
 
 export default (router: Router) : void => {
-    router.post('/signup', (req, res) => {
-        res.json({ 
-            ok: 'ok'
-        })
-    })
+    router.post('/signup', )
 }
