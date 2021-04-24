8353b3ca87d7a4a0d1ea0b0c561b5a07c0db5dd5
willian mattos <willian_matt@hotmail.com>
2021-04-24 11:00:17 -0300

    refactor: remove ununsed tests

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,4 +1,4 @@
-import { InvalidParamError, MissingParamError } from "../../errors"
+import { InvalidParamError } from "../../errors"
 import { badRequest, ok, serverError } from "../../helpers/http-helper"
 import { Validation } from "../../helpers/validators/validation"
 import { Controller, EmailValidator, HttpRequest, HttpResponse , AddAccount} from "./signup-protocols"
@@ -20,12 +20,7 @@ export class SignUpController implements Controller {
             if(error) {
                 return badRequest(error)
             }
-            const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-            for (const field of requiredFields) {
-                if (!httpRequest.body[field]) {
-                    return badRequest(new MissingParamError(field))
-                }
-            }
+            
             const { name, email, password, passwordConfirmation } = httpRequest.body
             if (password !== passwordConfirmation) {
                 return badRequest(new InvalidParamError('passwordConfirmation'))
