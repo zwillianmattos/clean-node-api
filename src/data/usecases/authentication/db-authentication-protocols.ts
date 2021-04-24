736ee75308ac9559a2457f92bd1ed8df6318484e
willian mattos <willian_matt@hotmail.com>
2021-04-24 17:17:17 -0300

    refactor: move DbAuthentication protocols to a single file

---
+++ src/data/usecases/authentication/db-authentication-protocols.ts
@@ -0,0 +1,8 @@
+
+export * from "../../../domain/models/account"
+export * from "../../../domain/usercases/authentication"
+export * from "../../protocols/criptography/hash-comparer"
+export * from "../../protocols/criptography/token-generator"
+export * from "../../protocols/db/load-account-by-email-repository"
+export * from "../../protocols/db/update-access-token-repository"
+export *  from "./db-authentication"
