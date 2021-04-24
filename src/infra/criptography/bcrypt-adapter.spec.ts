3657434502177b43aeffeda40186558d2e3461d7
willian mattos <willian_matt@hotmail.com>
2021-04-24 18:08:21 -0300

    test: ensure BcryptAdapter returns false when compare fai

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -52,4 +52,13 @@ describe('Bcrypt Adapter', () => {
         const isValid = await sut.compare('any_value', 'any_hash')
         expect(isValid).toBe(true)
     })
+
+    test('Shold return false when compare succeds', async () => {
+        const sut = makeSut()
+        jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(
+            new Promise((resolve) => resolve(false))
+        )
+        const isValid = await sut.compare('any_value', 'any_hash')
+        expect(isValid).toBe(false)
+    })
 })
